94ced526762b97ae68ffd32a771aba1e07f447b7
Rodrigo Rafael Flores Álvarez <250301052@ucaribe.edu.mx>
2026-05-18 18:36:58 -0500

    Update pagina_web.py

--- pagina_web.py
+++ pagina_web.py
@@ -12,3 +12,16 @@ if __name__ == "__main__":
 mkdir templates; mkdir static; mkdir static/css
 ni index.html
 
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h2>Hello World Medium</h2>
+</body>
+</html>
+
+
